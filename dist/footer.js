62176412dd147fa388dd03119ee38399feefa986
Ivan Miranda <alfa02ruizmiranda@gmail.com>
2024-04-28 16:41:08 -0300

    ultimo commit

---
+++ dist/footer.js
@@ -0,0 +1,7 @@
+export function footerComponent() {
+    const footer = document.createElement("footer");
+    footer.innerHTML = "<h2>Contactos</h2>";
+    footer.style.border = "solid 3px black";
+    footer.style.padding = "10px";
+    return footer;
+}
